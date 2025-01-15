065420735c9fc783c589fe2971f2c40a5173cdd2
matsvanspringel <r1035592@student.thomasmore.be>
2025-01-15 10:31:05 +0100

    Product pagina aangemaakt + footer gemaakt

--- script.js
+++ script.js
@@ -132,7 +132,7 @@ function get_spotlights(category){
             div.append(h3);
             div.append(p);
 
-            div.classList.add('product', 'col-3', 'm-2')
+            div.classList.add('product','col-md-3','my-2')
 
             //append div to productscontainer
             container.append(div);
